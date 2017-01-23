eb05332f960e58b530876529b0744f769666f861
sytyuk <yura.sytyuk@yandex.ua>
2017-01-23 12:23:18 +0200

    Change Theme and size button

--- app/src/main/java/ua/lviv/kreatech/stopwatch/StopwatchActivity.java
+++ app/src/main/java/ua/lviv/kreatech/stopwatch/StopwatchActivity.java
@@ -1,14 +1,14 @@
 package ua.lviv.kreatech.stopwatch;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class StopwatchActivity extends AppCompatActivity {
+public class StopwatchActivity extends Activity {
 
     private long seconds = 0L;
     private boolean running;
